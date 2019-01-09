2e6772c6cf13a62595b485ffb41ae0efc41d1221
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-01-09 09:56:55 +0100

    Preparing release 1.0.0

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.0.0 (unreleased)
+1.0.0 (2019-01-09)
 ------------------
 
 - Remove custom persistent field from registry.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.6.2.dev0'
+version = '1.0.0'
 
 tests_require = [
     'plone.app.testing',
