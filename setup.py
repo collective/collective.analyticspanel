84b81765f507c70321b6701c97ae98a79d00a3bf
Andrea Cecchi <andrea.cecchi85@gmail.com>
2022-11-23 09:59:28 +0100

    fix classifiers

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0.0'
+version = "2.0.0"
 
 tests_require = [
-    'plone.app.testing',
-    'plone.app.contenttypes',
-    'plone.app.robotframework[debug]',
-    'unittest2',
+    "plone.app.testing",
+    "plone.app.contenttypes",
+    "plone.app.robotframework[debug]",
+    "unittest2",
 ]
 
 setup(
-    name='collective.analyticspanel',
+    name="collective.analyticspanel",
     version=version,
     description="Add more control over the inclusion of JavaScript analytics code of your Plone site",
     long_description=open("README.rst").read()
@@ -24,25 +24,25 @@ setup(
         "Framework :: Plone",
         "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
-        "Programming Language :: Python 3.8",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords='plone analytics plonegov',
-    author='RedTurtle Technology',
-    author_email='sviluppoplone@redturtle.it',
-    url='http://plone.org/products/collective.analyticspanel',
-    license='GPL',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['collective'],
+    keywords="plone analytics plonegov",
+    author="RedTurtle Technology",
+    author_email="sviluppoplone@redturtle.it",
+    url="http://plone.org/products/collective.analyticspanel",
+    license="GPL",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["collective"],
     include_package_data=True,
     zip_safe=False,
     tests_require=tests_require,
     extras_require=dict(test=tests_require),
     install_requires=[
-        'setuptools',
-        'collective.z3cform.datagridfield',
-        'plone.app.registry',
-        'plone.app.vocabularies',
-        'plone.api',
+        "setuptools",
+        "collective.z3cform.datagridfield",
+        "plone.app.registry",
+        "plone.app.vocabularies",
+        "plone.api",
     ],
     entry_points="""
       [z3c.autoinclude.plugin]
